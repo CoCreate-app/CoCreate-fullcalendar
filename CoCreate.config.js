87310f8830054fed5dd7f52c8a1389d352dd608c
frankpagan <frank@cocreate.app>
2023-09-17 19:11:37 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,18 +8,17 @@ module.exports = {
             "object": {
                 "_id": "6028368f71064611a678a1a3",
                 "name": "index.html",
-                "path": "/docs/fullcalendar/index.html",
+                "path": "/docs/fullcalendar",
+                "pathname": "/docs/fullcalendar/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/fullcalendar",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "fullcalendar",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
-}
+};
