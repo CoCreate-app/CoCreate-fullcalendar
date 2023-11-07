0fc38360f38fd4752c3ea3546cfd9d9b01ce7109
frankpagan <frank@cocreate.app>
2023-11-07 09:47:45 -0600

    fix: update host

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -12,8 +12,7 @@ module.exports = {
                 "pathname": "/docs/fullcalendar/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
-                    "*",
-                    "general.cocreate.app"
+                    "*"
                 ],
                 "directory": "fullcalendar",
                 "content-type": "{{content-type}}",
